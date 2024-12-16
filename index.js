9cd77608d949ae5b510862d67fbddbb6fc28f3a7
Amy <amy.gh.ly@gmail.com>
2024-12-16 12:47:41 +1100

    add function to convert digit hour and minute to hour and minute in english

--- index.js
+++ index.js
@@ -5,7 +5,109 @@ function convertTimeToWords(time) {
     return 'midnight';
   }
 
-  return 'half past eight';
+  if (time === '12:00'){
+    return 'midday'
+  }
+
+  let processed = time.split(":")
+
+  // [0, 00]
+
+  let hour = processed[0]
+
+  let minute = processed[1]
+
+  let hourEng = convertHourToWord(parseInt(hour))
+
+  let minuteEng
+
+  let type 
+
+  let nextHour = convertHourToWord(parseInt(hour) + 1)
+
+  if (parseInt(minute) <= 30){
+    minuteEng = minuteMap.get(parseInt(minute))
+    type = "past"
+  }
+  else{
+    let diff = minuteDiff(minute)
+    minuteEng = minuteMap.get(diff)
+    type = "to"
+  }
+
+  
+  return `${minuteEng} ${type} ${hourEng}`
+  
+}
+
+const minuteMap = new Map([
+  [0, "o'clock"],
+  [1, "one"],
+  [2, "two"],
+  [3, "three"],
+  [4, "four"],
+  [5, "five"],
+  [6, "six"],
+  [7, "seven"],
+  [8, "eight"],
+  [9, "nine"],
+  [10, "ten"],
+  [11, "eleven"],
+  [12, "twelve"],
+  [13, "thirteen"],
+  [14, "fourteen"],
+  [15, "quarter"],
+  [16, "sixteen"],
+  [17, "seventeen"],
+  [18, "eighteen"],
+  [19, "nineteen"],
+  [20, "twenty"],
+  [21, "twenty one"],
+  [22, "twenty two"],
+  [23, "twenty three"],
+  [24, "twenty four"],
+  [25, "twenty five"],
+  [26, "twenty six"],
+  [27, "twenty seven"],
+  [28, "twenty eight"],
+  [29, "twenty nine"],
+  [30, "half"]
+]);
+
+const hourMap = new Map ([
+  [1, 'one'],
+  [2, 'two'],
+  [3, 'three'],
+  [4, 'four'],
+  [5, 'five'],
+  [6, 'six'],
+  [7, 'seven'],
+  [8, 'eight'],
+  [9, 'nine'],
+  [10, 'ten'],
+  [11, 'eleven'],
+  [12, 'twelve']
+])
+
+function minuteDiff(minute){
+  let diff = 60 - minute
+
+  return (
+    diff
+  )
+}
+
+function convertHourToWord(hour){
+  if (hour > 12){
+    return (
+      "Invalid hour"
+    )
+  }
+  else {
+    return (
+      hourMap.get(hour)
+    )
+  }
 }
 
 module.exports = { convertTimeToWords };
